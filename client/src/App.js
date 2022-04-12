577022ccce8213f69edae0fc09f268e7810e9010
JesusGambaro <gambaropablo4@gmail.com>
2022-04-12 18:51:39 -0300

    m

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,18 @@ import Shoes from "./Components/Shoes";
 import {Route, Routes} from "react-router-dom";
 import NotFound from "./Components/NotFound";
 import NavBar from "./Components/NavBar";
-
+import LandingPage from "./Components/LandingPage";
 function App() {
   return (
     <Routes>
       <Route
         exact
         path="/"
+        element={[<NavBar key={"nav"} />, <LandingPage key={"landing"} />]}
+      />
+      <Route
+        exact
+        path="/home"
         element={[<NavBar key={"nav"} />, <Shoes key={"shoes"} />]}
       />
       <Route path="*" exact={true} element={<NotFound />} />

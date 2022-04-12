e130c56e44d30cb89ca5f295f92c384c2dac573e
santiago altamira <santiagoaltamira@gmail.com>
2022-04-12 17:54:49 -0300

    Terminada la ruta GET/allCategories

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,10 @@
 const {Router} = require('express');
+const allCategories = require('./categories.js');
 
 const router = Router();
 
 // router.use('/shoes', shoesRoutes);
 // router.use('/users', usersRoutes);
+router.use('/allCategories', allCategories);
 
 module.exports = router;

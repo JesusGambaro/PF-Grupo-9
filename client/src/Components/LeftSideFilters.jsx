3b42b9e3af21288a4bc282eb47ab1ae44d88c3eb
AgustinRecabarren <92731295+AgusReka@users.noreply.github.com>
2022-04-12 16:54:14 -0300

    0

--- client/src/Components/LeftSideFilters.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../Css/leftside.scss";
-const LeftSideFilters = () => {
-  return <div className="left-side-container">LeftSideFilters</div>;
-};
-
-export default LeftSideFilters;

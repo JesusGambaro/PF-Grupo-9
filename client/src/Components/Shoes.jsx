3521a9ba434081460b82d6054de68a9e33cb8aea
JesusGambaro <gambaropablo4@gmail.com>
2022-04-13 22:50:37 -0300

    Home-Detail-Landing v1

--- client/src/Components/Shoes.jsx
+++ client/src/Components/Shoes.jsx
@@ -1,58 +1,14 @@
-import Card from "./Card";
-import {useEffect, useRef, useState} from "react";
-import axios from "axios";
-import Loading from "./Loading";
-import LeftSideFilters from "./LeftSideFilters";
-import UpSideBar from "./UpSideBar";
+import {useEffect} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import Pagination from "./Pagination";
+import bringAllData from "../Redux/actions/bringAllData";
 const Shoes = () => {
-  const [info, setInfo] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const start = useRef(false);
-  const [toggle, setToggle] = useState(true);
+  const dispatch = useDispatch();
+  const shoes = useSelector((state) => state);
   useEffect(() => {
-    (async () => {
-      setLoading(true);
-      const {data} = await axios.get("http://localhost:3001/sneaks");
-      setInfo(data);
-      start.current = true;
-      setLoading(false);
-    })();
-  }, [start]);
+    if (!shoes.allData.length) dispatch(bringAllData());
+  }, []);
 
-
-  const scrollBar = document.getElementById("scroll-bar");
-  console.log(scrollBar);
-  const capo = document.getElementById("capo");
-  console.log(capo);
-  const height =
-    document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  
-  window.addEventListener("scroll", () => {
-    const scrollTop =
-      document.body.scrollTop || document.documentElement.scrollTop;
-    scrollBar.style.height = `${(scrollTop / height) * 100}%`;
-  });
-  
-
-
-
-
-
-  return (
-    <div className="home-container">
-     <div id="scroll-bar"></div>
-      <UpSideBar quantity={info.length} handleToggle={(p) => setToggle(p)} />
-      <LeftSideFilters />
-      {loading ? (
-        <Loading />
-      ) : (
-        <div className={"shoes-container" + (toggle ? "" : " h")}>
-          {info.map((e, i) => (
-            <Card e={e} key={i} horizontal={!toggle} />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return <Pagination shoes={shoes} pageLimit={4} cardsPerPage={12} />;
 };
 export default Shoes;
--- client/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import rootReducer from "./reducers/reducer";
-import { createStore, applyMiddleware} from "redux";
-import { composeWithDevTools } from "redux-devtools-extension"
-import thunk from "redux-thunk"
-
-const store = createStore(rootReducer,
-    composeWithDevTools(applyMiddleware(thunk)));
-export default store;

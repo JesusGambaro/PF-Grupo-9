30fa76183a8214740472d1022876335c63a98b06
JesusGambaro <gambaropablo4@gmail.com>
2022-04-14 18:47:34 -0300

    Minor errors

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
-import '../Css/Footer.css'
-import logo from "../Images/logo2.png"
+import React from "react";
+import "../Css/Footer.css";
+import logo from "../Images/logo2.png";
 
-export default function Footer(){
-    return(
-        
-        <footer className='footer-container'>
-            <div className='footer'>
-            <img className="logoOwn" src={logo} alt="logo" />
+export default function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer">
+        <img className="logoOwn" src={logo} alt="logo" />
 
-                <div>información de contácto:
-                    <li className='itemL'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed di.</li>
-                    <li className='itemL'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed di.</li>
-                    <li className='itemL'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed di.</li>
-                </div>
-                <div>medios de pago:
-                <li className='itemL'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed di.</li>
-                </div>
-            </div>
-        </footer>
-       
-    )
+        <div>
+          información de contácto:
+          <li className="itemL">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed di.
+          </li>
+          <li className="itemL">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed di.
+          </li>
+          <li className="itemL">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed di.
+          </li>
+        </div>
+        <div>
+          medios de pago:
+          <li className="itemL">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed di.
+          </li>
+        </div>
+      </div>
+    </footer>
+  );
 }

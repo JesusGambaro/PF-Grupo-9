9094ea3f13dcfd2ed8bb847c7cefc63ff2daa72b
lau-urajbm <laurajbm1@gmail.com>
2022-04-20 18:08:10 -0500

    se agrego input de busqueda para buscar un pedido específico y ahora se muestra el valor real del número de productos

--- client/src/Components/AdminPandO.jsx
+++ client/src/Components/AdminPandO.jsx
@@ -19,7 +19,17 @@ export default function AdminPandO() {
 
       <div className='orders-card'>
         <div className='lastest-orders-body-card'>
+        <div className='title-and-search'>
         <h3 className='t-title'>Orders</h3>
+        <form className=" col-md-6 admin-search">
+        
+						<input type="text" placeholder="Search..." class="form-control-admin"></input>
+            <button type="submit" className='submit-admin'>
+        <i className="bi bi-search search-admin"></i>
+        </button>
+					</form>
+          </div>
+        
         <table class="table table-hover">
         <thead>
 						<tr>
@@ -29,7 +39,7 @@ export default function AdminPandO() {
 							<th scope="col">Total</th>
 							<th scope="col">Status</th>
 							<th scope="col">Date</th>
-							<th scope="col" class="text-end"> Action </th>
+							<th scope="col" className="text-end"> Action </th>
 						</tr>
 					</thead>
           <tbody>
@@ -40,7 +50,7 @@ export default function AdminPandO() {
           			<td>$778.35</td>
           			<td><span class="badge rounded-pill alert-success">status</span></td>
           			<td>07.05.2020</td>
-          			<td class="text-end">
+          			<td className="text-end">
           				<a href="#" class="btn btn-light">Detail</a>
           			</td>
           		</tr>

818f2c0d61b45c360fa0dd82884b91b0a04aff7f
santiago altamira <santiagoaltamira@gmail.com>
2022-04-20 14:55:39 -0300

    Changes on db and fixes in controllers orders and shoppingCart

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -2,15 +2,24 @@ const { DataTypes } = require("sequelize")
 
 module.exports = (sequelize) => {
   sequelize.define("order", {
+    size: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     delivered: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
     },
     address: {
         type: DataTypes.TEXT,
-      },
-      telephoneNum: {
-        type: DataTypes.INTEGER,
-      },
+        allowNull: false,
+    },
+    telephoneNum: {
+      type: DataTypes.INTEGER,
+    },
   })
 }

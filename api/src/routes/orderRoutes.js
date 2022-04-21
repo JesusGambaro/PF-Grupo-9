c724b09f6851846d6b87a97e3a27fe269012681d
santiago altamira <santiagoaltamira@gmail.com>
2022-04-21 10:18:20 -0300

    Fixing diferences

--- api/src/routes/orderRoutes.js
+++ api/src/routes/orderRoutes.js
@@ -1,13 +1,21 @@
-const { Router } = require('express');
-const {getOrders, postOrder, putOrder, deleteOrder, getLastSevenDaysOrders} = require('../controllers/order.js');
+const { Router } = require("express")
+const {
+  getOrders,
+  postOrder,
+  putOrder,
+  deleteOrder,
+  getLastSevenDaysOrders,
+} = require("../controllers/order.js")
+const { verifyToken } = require("../middlewares/auth.js")
 
-const router = Router();
+const router = Router()
 
-router.route('/')
-     .get(getOrders)
-     .post(postOrder)
-     .put(putOrder)
-     .delete(deleteOrder);
-router.get("/ByDate", getLastSevenDaysOrders);
-     
-module.exports = router;
+router
+  .route("/")
+  .get(verifyToken, getOrders)
+  .post(verifyToken, postOrder)
+  .put(verifyToken, putOrder)
+  .delete(verifyToken, deleteOrder)
+router.get("/ByDate", verifyToken, getLastSevenDaysOrders)
+
+module.exports = router
--- api/src/routes/shoppingCartRoutes.js
+++ api/src/routes/shoppingCartRoutes.js
@@ -1,16 +1,19 @@
-const {Router} = require("express");
-const {getCart, deleteCart, putCart, postCart} = require("../controllers/shoppingCart.js");
 
-const router = Router();
+const { Router } = require("express")
+const {
+  getCart,
+  deleteCart,
+  putCart,
+  postCart,
+} = require("../controllers/shoppingCart.js")
+const { verifyToken } = require("../middlewares/auth.js")
 
-// router.get("/cart", getCart);
-// router.delete("/cart", deleteCart);
-// router.put("/cart", putCart);
-// router.post("/cart", postCart);
-router.route('/')
-  .get(getCart)
-  .delete(deleteCart)
-  .put(putCart)
-  .post(postCart);
+const router = Router()
+router
+  .route("/")
+  .get(verifyToken, getCart)
+  .delete(verifyToken, deleteCart)
+  .put(verifyToken, putCart)
+  .post(verifyToken, postCart)
 
-module.exports = router;
+module.exports = router

64dcd592a7b7da2b8e8927f9a785cde1f9728db1
NachoPer <nachopereee@gmail.com>
2022-04-22 20:57:39 -0300

    Fix rute gett orders now filter for email and delivered

--- api/src/routes/orderRoutes.js
+++ api/src/routes/orderRoutes.js
@@ -7,6 +7,7 @@ const {
   getLastSevenDaysOrders,
   getOrdersUser,
   getTotalGain,
+  getTotalOrders,
 } = require("../controllers/order.js")
 const {
   verifyTokenUserOrAdmin,
@@ -15,14 +16,22 @@ const {
 
 const router = Router()
 
-router
-  .route("/")
-  .get(verifyTokenUserOrAdmin, getOrders)
-  .post(verifyTokenUserOrAdmin, postOrder)
+// router
+//   .route("/")
+//   .get(verifyTokenUserOrAdmin, getOrders)
+//   .post(verifyTokenUserOrAdmin, postOrder)
+// router.get("/userOrders/:userId", getOrdersUser)
+// router.put("/:id", verifyTokenAdmin, putOrder)
+// router.delete("/:id", verifyTokenAdmin, deleteOrder)
+// router.get("/ByDate", verifyTokenAdmin, getLastSevenDaysOrders)
+// router.get("/totalGain", getTotalGain)
+
+router.route("/").get(getOrders).post(postOrder)
 router.get("/userOrders/:userId", getOrdersUser)
-router.put("/:id", verifyTokenAdmin, putOrder)
-router.delete("/:id", verifyTokenAdmin, deleteOrder)
-router.get("/ByDate", verifyTokenAdmin, getLastSevenDaysOrders)
+router.put("/:id", putOrder)
+router.delete("/:id", deleteOrder)
+router.get("/ByDate", getLastSevenDaysOrders)
 router.get("/totalGain", getTotalGain)
+router.get("/totalOrders", getTotalOrders)
 
 module.exports = router

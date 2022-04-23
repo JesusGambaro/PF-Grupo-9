cd66813021dee0fe322fc6feebb427c431c6c814
NachoPer <nachopereee@gmail.com>
2022-04-23 12:16:02 -0300

    Feat: add route for cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import "./Css/app.scss";
-import Shoes from "./Components/Shoes";
-import {Route, Routes} from "react-router-dom";
-import NotFound from "./Components/NotFound";
-import NavBar from "./Components/NavBar";
-import LandingPage from "./Components/LandingPage";
-import Details from "./Components/Details";
-import Footer from "./Components/Footer";
-import Login from "./Components/Login";
-import "bootstrap/dist/css/bootstrap.min.css";
-import AdminDashboard from "./Components/Admin/AdminDashboard";
-import AdminPandO from "./Components/Admin/AdminPandO";
-import AdminProducts from "./Components/Admin/AdminProducts";
-import AdminCustomers from "./Components/Admin/AdminCustomers";
-import AdminOrderDetail from "./Components/Admin/AdminOrderDetail";
-
+import "./Css/app.scss"
+import Shoes from "./Components/Shoes"
+import { Route, Routes } from "react-router-dom"
+import NotFound from "./Components/NotFound"
+import NavBar from "./Components/NavBar"
+import LandingPage from "./Components/LandingPage"
+import Details from "./Components/Details"
+import Footer from "./Components/Footer"
+import Login from "./Components/Login"
+import "bootstrap/dist/css/bootstrap.min.css"
+import AdminDashboard from "./Components/Admin/AdminDashboard"
+import AdminPandO from "./Components/Admin/AdminPandO"
+import AdminProducts from "./Components/Admin/AdminProducts"
+import AdminCustomers from "./Components/Admin/AdminCustomers"
+import AdminOrderDetail from "./Components/Admin/AdminOrderDetail"
+import Cart from "./Components/Cart"
 
 function App() {
   return (
@@ -41,6 +41,15 @@ function App() {
           <Footer key={"footer"} />,
         ]}
       />
+      <Route
+        exact
+        path="home/cart"
+        element={[
+          <NavBar key={"nav"} />,
+          <Cart key={"cart"} />,
+          <Footer key={"footer"} />,
+        ]}
+      />
 
       <Route exact path="home/admin/dashboard" element={<AdminDashboard />} />
       <Route exact path="home/admin/products" element={<AdminProducts />} />
@@ -64,7 +73,7 @@ function App() {
 
       <Route path="*" exact={true} element={<NotFound />} />
     </Routes>
-  );
+  )
 }
 
-export default App;
+export default App

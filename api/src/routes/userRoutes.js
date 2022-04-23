c78c0c5d43b4887cbf190be08753073df1429ed9
Christian Sosa <chrissm28@outlook.com>
2022-04-23 14:47:19 -0500

    Added Reset Password with Mailtrap, Added Token to User Model and two helpers

--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const {
   userSingUp,
   userSingIn,
+  forgotPassword,
+  changePassword,
+  changeForgottenPassword,
   getAllUsers,
   deleteUser,
   getRole,
@@ -14,6 +17,9 @@ const router = require("express").Router()
 
 router.post("/signUp", userSingUp)
 router.post("/signIn", userSingIn)
+router.post("/forgot-password", forgotPassword)
+router.post("/forgot-password/:token", changeForgottenPassword)
+router.post("/change-password", changePassword)
 router.get("/allUsers", verifyTokenAdmin, getAllUsers)
 router.delete("/deleteUser", verifyTokenAdmin, deleteUser)
 router.get("/role", verifyTokenUserOrAdmin, getRole)

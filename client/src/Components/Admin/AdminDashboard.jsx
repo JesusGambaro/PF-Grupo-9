981def4485f6ff434327807eaa189ca46cc73744
NachoPer <nachopereee@gmail.com>
2022-04-24 11:19:38 -0300

    a

--- client/src/Components/Admin/AdminDashboard.jsx
+++ client/src/Components/Admin/AdminDashboard.jsx
@@ -4,8 +4,6 @@ import bringAllData from "../../redux/actions/bringAllData";
 import {useEffect} from "react";
 import { useNavigate, NavLink } from "react-router-dom"
 import {useSelector, useDispatch} from "react-redux";
-import "../../Css/AdminDashboard.css";
-import {useNavigate, NavLink} from "react-router-dom";
 import {
   getAllOrders,
   getLastSevenDaysOrders,
@@ -18,7 +16,7 @@ export default function AdminDashboard() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const shoes = useSelector((state) => state.admin.allData.length);
-  const {role} = useSelector((store) => store.root);
+
 
   useEffect(() => {
     if (!shoes.length) dispatch(bringAllData(true));

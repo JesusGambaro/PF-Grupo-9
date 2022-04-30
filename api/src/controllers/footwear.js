37067ce7b4965f67fd57f42e0c7472ed480d2ab1
santiago altamira <santiagoaltamira@gmail.com>
2022-04-30 17:45:45 -0300

    changed controller footwear

--- api/src/controllers/footwear.js
+++ api/src/controllers/footwear.js
@@ -1,25 +1,19 @@
 const { Op, Sequelize } = require("sequelize")
-const { Product, Image, Stock, Order, ShoppingCartItem, FavoriteItem } = require("../db.js")
+const {
+  Product,
+  Image,
+  Stock,
+  ShoppingCartItem,
+  FavoriteItem,
+  Review,
+} = require("../db.js")
 const { sendError } = require("../helpers/error.js")
+const cloudinary = require("../helpers/cloudinary.js")
 
 module.exports = {
   getAllFootwear: async (req, res) => {
     const { footwear } = req.query
     try {
-      const allFootwears = await Product.findAll({
-        attributes: { exclude: "description" },
-        include: [
-          {
-            model: Image,
-          },
-          {
-            model: Stock,
-            where: {
-              amount: { [Op.gt]: 0 },
-            },
-          },
-        ],
-      })
       if (footwear) {
         const footwearsSearched = await Product.findAll({
           where: {
@@ -44,10 +38,70 @@ module.exports = {
               },
             },
           ],
+          order: [
+            ["id", "ASC"],
+            ["images", "id", "ASC"],
+          ],
+        })
+        return res.send(footwearsSearched)
+      }
+      const allFootwears = await Product.findAll({
+        attributes: { exclude: "description" },
+        include: [
+          {
+            model: Image,
+          },
+          {
+            model: Stock,
+            where: {
+              amount: { [Op.gt]: 0 },
+            },
+          },
+        ],
+        order: [
+          ["id", "ASC"],
+          ["images", "id", "ASC"],
+        ],
+      })
+      return res.send(allFootwears)
+    } catch (error) {
+      sendError(res, error)
+    }
+  },
+
+  getAllFootwearForAdmin: async (req, res) => {
+    const { footwear } = req.query
+    try {
+      if (footwear) {
+        const footwearsSearched = await Product.findAll({
+          where: {
+            [Op.or]: [
+              { model: { [Op.iLike]: `%${footwear}%` } },
+              Sequelize.where(
+                Sequelize.cast(Sequelize.col("brand"), "varchar"),
+                {
+                  [Op.iLike]: `%${footwear}%`,
+                }
+              ),
+            ],
+          },
+          include: [{ model: Image }, { model: Stock }],
+          order: [
+            ["id", "ASC"],
+            ["images", "id", "ASC"],
+          ],
         })
 
         return res.send(footwearsSearched)
       }
+      const allFootwears = await Product.findAll({
+        attributes: { exclude: "description" },
+        include: [{ model: Image }, { model: Stock }],
+        order: [
+          ["id", "ASC"],
+          ["images", "id", "ASC"],
+        ],
+      })
       return res.send(allFootwears)
     } catch (error) {
       sendError(res, error)
@@ -85,9 +139,21 @@ module.exports = {
         where: {
           sale: { [Op.gt]: 0 },
         },
-        include: {
-          model: Image,
-        },
+        include: [
+          {
+            model: Image,
+          },
+          {
+            model: Stock,
+            where: {
+              amount: { [Op.gt]: 0 },
+            },
+          },
+        ],
+        order: [
+          ["id", "ASC"],
+          ["images", "id", "ASC"],
+        ],
       })
 
       res.send(carouselSale)
@@ -111,10 +177,36 @@ module.exports = {
           },
           {
             model: Stock,
-            where: {
-              amount: { [Op.gt]: 0 },
-            },
           },
+          {
+            model: Review,
+            through: { attributes: [] },
+          },
+        ],
+        order: [
+          ["id", "ASC"],
+          ["images", "id", "ASC"],
+        ],
+      })
+      res.status(200).send(productsSearched)
+    } catch (error) {
+      sendError(res, error)
+    }
+  },
+
+  getAllProductsSameModelForAdmin: async (req, res) => {
+    try {
+      const { model } = req.params
+      const productsSearched = await Product.findAll({
+        where: {
+          model: {
+            [Op.eq]: model,
+          },
+        },
+        include: [{ model: Image }, { model: Stock }],
+        order: [
+          ["id", "ASC"],
+          ["images", "id", "ASC"],
         ],
       })
       res.status(200).send(productsSearched)
@@ -126,7 +218,6 @@ module.exports = {
   getProductById: async (req, res) => {
     try {
       const { id } = req.params
-      // footwear es el calzado encontrado, findByPk retorna el coincidente con el id
       const footwear = await Product.findOne({
         where: {
           id: { [Op.eq]: id },
@@ -137,13 +228,36 @@ module.exports = {
           },
           {
             model: Stock,
-            where: {
-              amount: { [Op.gt]: 0 },
-            },
+          },
+          {
+            model: Review,
+            through: { attributes: [] },
           },
         ],
+        order: [
+          ["id", "ASC"],
+          ["images", "id", "ASC"],
+        ],
       })
-      // Retorna el coincidente. Si no existe, retorna un array vacio
+      res.send(footwear)
+    } catch (error) {
+      sendError(res, error)
+    }
+  },
+
+  getProductByIdForAdmin: async (req, res) => {
+    try {
+      const { id } = req.params
+      const footwear = await Product.findOne({
+        where: {
+          id: { [Op.eq]: id },
+        },
+        include: [{ model: Image }, { model: Stock }],
+        order: [
+          ["id", "ASC"],
+          ["images", "id", "ASC"],
+        ],
+      })
       res.send(footwear)
     } catch (error) {
       sendError(res, error)
@@ -161,15 +275,26 @@ module.exports = {
         description,
         sale,
         color,
-        images,
         stock,
       } = req.body
 
+      // const model = "Harmoso3"
+      // const brand = "New Balance"
+      // const category = "Elegant"
+      // const gender = "Male"
+      // const price = 34000
+      // const description = "Zapato fachero"
+      // const sale = 0
+      // const color = "Yellow"
+      // const stock = [{size: 10, amount: 100}, {size:12, amount: 200}]
+
+      const imgFiles = req.files
+      
       const foundProduct = await Product.findOne({
-        where: {model, brand, color }
+        where: { model, brand, color },
       })
 
-      if(foundProduct) return res.send({msg:"This product already exist"})
+      if (foundProduct) return res.send({ msg: "This product already exist" })
       let product = await Product.create({
         model,
         brand,
@@ -181,13 +306,13 @@ module.exports = {
         color,
       })
 
-      images.length > 0 &&
-        images.map(async (im) => {
-          if(im.url.length > 0){
-            let imageProduct = await Image.create({ url: im.url })
-            await product.addImage(imageProduct)
-          }
+      if (Object.keys(imgFiles).length !== 0) {
+        Object.entries(imgFiles).forEach(async ([key, imgFile]) => {
+          const urlImg = await cloudinary(imgFile.tempFilePath)
+          let imageProduct = await Image.create({ url: urlImg.secure_url })
+          await product.addImage(imageProduct)
         })
+      }
 
       stock.length > 0 &&
         stock.map(async (amountAndSize) => {
@@ -197,6 +322,7 @@ module.exports = {
           })
           await product.addStock(stockProduct)
         })
+
       return res.send("Product with its images created!")
     } catch (error) {
       sendError(res, error)
@@ -214,12 +340,21 @@ module.exports = {
         description,
         sale,
         color,
-        size,
-        amount,
-        images,
-      
+        stock,
       } = req.body
+      // console.log("req.body", req.body)
+      // const model = "Harmoso3"
+      // const brand = "New Balance"
+      // const category = "Elegant"
+      // const gender = "Male"
+      // const price = 34000
+      // const description = "Zapato fachero"
+      // const sale = 0
+      // const color = "Yellow"
+      // const stock = [{size: 10, amount: 10000}, {size:12, amount: 20000}]
+
       const { id } = req.params
+      const imgFiles = req.files
 
       const product = await Product.findOne({
         where: { id },
@@ -257,26 +392,27 @@ module.exports = {
         product.color = color
         await product.save()
       }
-      if (size && amount) {
-        const stockPerModel = await Stock.findAll({
+      if (stock.length > 0) {
+        await Stock.destroy({
           where: { productId: product.id },
         })
-        stockPerModel.map((stockPerSize) => {
-          if (stockPerSize.size === size) {
-            stockPerSize.amount = amount
-            stockPerSize.save()
-          }
+        stock.map(async (amountAndSize) => {
+          let stockProduct = await Stock.create({
+            size: parseInt(amountAndSize.size),
+            amount: parseInt(amountAndSize.amount),
+          })
+          await product.addStock(stockProduct)
         })
       }
-      if (images) {
+
+      if (Object.keys(imgFiles).length !== 0) {
         const productImages = await Image.destroy({
-          where: {productId: id}
-        });
-        images.map(async (im) => {
-          if(im.url.length > 0){
-            let imageProduct = await Image.create({ url: im.url })
-            await product.addImage(imageProduct)
-          }
+          where: { productId: id },
+        })
+        Object.entries(imgFiles).forEach(async ([key, imgFile]) => {
+          const urlImg = await cloudinary(imgFile.tempFilePath)
+          let imageProduct = await Image.create({ url: urlImg.secure_url })
+          await product.addImage(imageProduct)
         })
       }
 
@@ -323,11 +459,11 @@ module.exports = {
           where: { productId: id },
         })
         favoriteItem &&
-        favoriteItem.map(async (favItem) => {
+          favoriteItem.map(async (favItem) => {
             await favItem.destroy()
           })
 
-        // Finalmente elimimnar el producto.  
+        // Finalmente elimimnar el producto.
         product.destroy()
       }
       res.send("product destroyed")

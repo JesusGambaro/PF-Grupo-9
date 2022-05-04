ba171dae53d6c615d339618661125e1f41ff6bde
JesusGambaro <gambaropablo4@gmail.com>
2022-05-04 17:07:11 -0300

    fix cookie

--- client/src/Components/LoginGoogle.jsx
+++ client/src/Components/LoginGoogle.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import { GoogleLogin } from 'react-google-login';
-import { loginGoogle } from "../redux/actions/Loginregister"
-import { useDispatch } from "react-redux"
-import Swal from "sweetalert2"
+import React from "react";
+import {GoogleLogin} from "react-google-login";
+import {loginGoogle} from "../redux/actions/Loginregister";
+import {useDispatch} from "react-redux";
+import Swal from "sweetalert2";
 
 function LoginGoogle() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const onSuccess = (response) => dispatch(loginGoogle({
-    email: response.profileObj.email,
-    username: response.profileObj.name
-  }))
+  const onSuccess = (response) =>
+    dispatch(
+      loginGoogle({
+        email: response.profileObj.email,
+        username: response.profileObj.name,
+      })
+    );
 
   const onFailure = () => {
     Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
+      icon: "error",
+      title: "Oops...",
       text: "We couldn't login!",
-    })
-  }
+    });
+  };
 
   return (
-    <div className='mt-4'>
+    <div className="mt-4">
       <GoogleLogin
         clientId="931352466233-7mhmtsa47dv0p2bpi59fsaakomg8fng0.apps.googleusercontent.com"
         disabled={false}
@@ -29,7 +32,7 @@ function LoginGoogle() {
         onSuccess={onSuccess}
         onFailure={onFailure}
         theme={"dark"}
-        cookiePolicy={'single_host_origin'}
+        cookiePolicy={"https://henry-shoes.vercel.app/"}
       />
     </div>
   );
